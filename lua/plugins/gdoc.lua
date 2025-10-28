f0f67935ce397c91de71541b395e7f025dbc91b9
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-10-28 20:54:08 +1100

    Update gdoc.lua

--- lua/plugins/gdoc.lua
+++ lua/plugins/gdoc.lua
@@ -2,7 +2,7 @@ return {
   {
     "Aadv1k/gdoc.vim",
     lazy = false, -- Set to false to ensure the plugin is loaded immediately on startup
-    cmd = "Gdoc", -- This is the command to start the Google Docs interface
+    -- cmd = "Gdoc", -- This is the command to start the Google Docs interface
   },
   -- ... other plugins ...
 }
